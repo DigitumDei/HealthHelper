130ac3248dd653991221d40087d82efd7be73447
Dion van Huyssteen <dion@darkervision.com>
2025-10-14 08:13:32 +0200

    Fix week view activation for shell navigation

--- HealthHelper/Pages/WeekViewPage.xaml.cs
+++ HealthHelper/Pages/WeekViewPage.xaml.cs
@@ -1,9 +1,15 @@
 using HealthHelper.PageModels;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace HealthHelper.Pages;
 
 public partial class WeekViewPage : ContentPage
 {
+    public WeekViewPage()
+        : this(((App)Application.Current!).Services.GetRequiredService<WeekViewModel>())
+    {
+    }
+
     public WeekViewPage(WeekViewModel viewModel)
     {
         InitializeComponent();
